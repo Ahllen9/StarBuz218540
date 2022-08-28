1efc57cc432bd941c5dbc4a89edc2f5998e40b59
Ahllen9 <ldoar99@gmail.com>
2022-08-27 20:27:19 -0700

    Construccion de clases

---
+++ src/Implementaciones/Expresso.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Implementaciones;
+
+import Interfaces.Bebida;
+
+/**
+ *
+ * @author arturo
+ */
+public class Expresso extends Bebida{
+    static double costo = 36.0;
+
+    public Expresso() {
+    }
+    
+    public static double getCosto() {
+        return costo;
+    }
+}
