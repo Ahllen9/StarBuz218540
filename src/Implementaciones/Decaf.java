ccd27478f2fe7c6182d762680d5b0f5d9db40a5f
Ahllen9 <ldoar99@gmail.com>
2022-08-27 20:28:54 -0700

    Cambio de nombre del paquete interfaces

--- src/Implementaciones/Decaf.java
+++ src/Implementaciones/Decaf.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Implementaciones;
-import Interfaces.Bebida;
+import Clases.Bebida;
 /**
  *
  * @author arturo
--- src/Implementaciones/Expresso.java
+++ src/Implementaciones/Expresso.java
@@ -4,7 +4,7 @@
  */
 package Implementaciones;
 
-import Interfaces.Bebida;
+import Clases.Bebida;
 
 /**
  *
